99556ca761d8fff2098537f35e8e9748d8c4afa5
Romain Crestey <rcrestey@gmail.com>
2020-12-09 08:34:33 +0100

    allow dead code in utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,24 @@
-// use std::fs::read_to_string;
+#![allow(dead_code)]
+
+use std::fs::read_to_string;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-// pub fn read_input(file_name: &str) -> Vec<String> {
-//   let file = File::open(file_name).expect("file not found!");
-//   let reader = BufReader::new(file);
+pub fn read_input(file_name: &str) -> Vec<String> {
+  let file = File::open(file_name).expect("file not found!");
+  let reader = BufReader::new(file);
 
-//   let mut strings = Vec::new();
+  let mut strings = Vec::new();
 
-//   for line in reader.lines() {
-//     let my_string = String::from(&line.unwrap());
+  for line in reader.lines() {
+    let my_string = String::from(&line.unwrap());
 
-//     strings.push(my_string);
-//   }
+    strings.push(my_string);
+  }
 
-//   return strings;
-// }
+  return strings;
+}
 
 pub fn read_input_as_nb(file_name: &str) -> Vec<i64> {
   let file = File::open(file_name).expect("file not found!");
@@ -33,19 +35,19 @@ pub fn read_input_as_nb(file_name: &str) -> Vec<i64> {
   return nbs;
 }
 
-// pub fn read_input_with_empty_lines(file_name: &str) -> Vec<String> {
-//   let input = read_to_string(file_name)
-//     .expect("file not found!")
-//     .to_string();
+pub fn read_input_with_empty_lines(file_name: &str) -> Vec<String> {
+  let input = read_to_string(file_name)
+    .expect("file not found!")
+    .to_string();
 
-//   let lines = input
-//     .split(
-//       "
+  let lines = input
+    .split(
+      "
 
-// ",
-//     )
-//     .map(|x| x.to_string())
-//     .collect();
+",
+    )
+    .map(|x| x.to_string())
+    .collect();
 
-//   return lines;
-// }
+  return lines;
+}
